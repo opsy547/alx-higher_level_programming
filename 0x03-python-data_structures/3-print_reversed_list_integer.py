4dfceb68f87ef15f338c22d4894a65994e7109a4
opeyemi <yemilaw547@gmail.com>
2023-03-13 12:34:06 -0700

    task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
-    if idx < 0 or idx >= len(my_list):
-        return(my_list)
-    else:
-        my_list[idx] = element
-        return(my_list)
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        my_list.reverse()
+        for x in range(len(my_list)):
+            print("{:d}".format(my_list[x]))
