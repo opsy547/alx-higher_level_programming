9e2ef71a0dc68c2d5b2c19d5832793f36aef9e5a
opeyemi <yemilaw547@gmail.com>
2023-03-15 10:31:36 -0700

    task 101

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -2,4 +2,4 @@
 # Opeeyemi Odedoyin
 
 def square_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))i
+    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
